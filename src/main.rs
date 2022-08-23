6599844a7c4625b48481d11e4d8da254841c0a2d
Chris Pryer <cnpryer@gmail.com>
2022-08-22 20:51:50 -0400

    Use LogCLI::from_clap

--- src/main.rs
+++ src/main.rs
@@ -86,10 +86,10 @@ fn main() {
 
     // Initialize the application.
     // TODO: LogCLI::with_clap(&matches);
-    let app = LogCLI::new(&matches);
+    let app = LogCLI::from_clap(&matches);
 
     // Attempt to display the contents otherwise print the error.
-    if let Err(e) = app.run(filepath) {
+    if let Err(e) = app.run_with_filepath(filepath) {
         eprintln!("Error: {:?}", e)
     }
 }
